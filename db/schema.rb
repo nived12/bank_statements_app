e2c6d318b5d44b0804d4c20ccd98cbf2eda9f87b
nived12 <nived.vengilat@apptegy.com>
2025-09-08 19:30:16 -0600

    Add OAuth authentication gems and database fields
    - Add omniauth, omniauth-google-oauth2, and omniauth-rails_csrf_protection gems
    - Add provider, uid, and avatar_url fields to users table
    - Add unique index on provider and uid combination
    - Update schema.rb with new fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_20_034555) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_09_004452) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -145,7 +145,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_20_034555) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "avatar_url"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"

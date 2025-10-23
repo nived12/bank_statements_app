da7bd83d87ba23d7ae5a516b36da052ab654de80
nived12 <nived.vengilat@apptegy.com>
2025-10-23 14:09:07 -0600

    Fix migration

--- db/migrate/20251022225258_create_debts.rb
+++ db/migrate/20251022225258_create_debts.rb
@@ -3,6 +3,7 @@ class CreateDebts < ActiveRecord::Migration[8.0]
     create_table :debts do |t|
       t.references :user, null: false, foreign_key: true
       t.references :category, null: true, foreign_key: true
+      t.references :goal, null: true, foreign_key: true
       t.references :bank_account, null: true, foreign_key: true
       t.string :name, null: false
       t.decimal :original_amount, precision: 12, scale: 2

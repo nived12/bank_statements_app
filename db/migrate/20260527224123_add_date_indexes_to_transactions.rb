58f00e9feb161815cb84badc65344f3130ad391a
Nived Vengilat <nivedvengilat@gmail.com>
2026-05-28 09:04:39 -0600

    Fix rubocop complaints

--- db/migrate/20260527224123_add_date_indexes_to_transactions.rb
+++ db/migrate/20260527224123_add_date_indexes_to_transactions.rb
@@ -1,6 +1,7 @@
 class AddDateIndexesToTransactions < ActiveRecord::Migration[8.0]
   def change
     add_index :transactions, [:user_id, :date], name: "index_transactions_on_user_id_and_date"
-    add_index :transactions, [:user_id, :bank_account_id, :date], name: "index_transactions_on_user_id_and_bank_account_id_and_date"
+    add_index :transactions, [:user_id, :bank_account_id, :date],
+      name: "index_transactions_on_user_id_and_bank_account_id_and_date"
   end
 end
